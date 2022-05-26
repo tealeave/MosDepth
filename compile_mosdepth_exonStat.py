36940b22d69b5961136657dcb3bbbb9dc43f5686
Da-Wei Lin <dlin@ambrygen.com>
2022-05-26 00:43:35 -0700

    pylint and update readme

--- compile_mosdepth_exonStat.py
+++ compile_mosdepth_exonStat.py
@@ -1,28 +1,39 @@
 import pandas as pd
-import sys, os, glob
+import sys, glob
 
-# This will glob all the region and threshold output from mosdepth
-region_glob = glob.glob('*.regions.bed.gz')
-threshold_glob = glob.glob('*.thresholds.bed.gz')
-summary_glob = glob.glob('*.mosdepth.summary.txt')
+"""This script globs MosDepth output and compile per target and threshold coverage for all samples"""
+
+region_glob = glob.glob("*.regions.bed.gz")
+threshold_glob = glob.glob("*.thresholds.bed.gz")
+summary_glob = glob.glob("*.mosdepth.summary.txt")
 cov_threshold = sys.argv[1]
 
+
 def read_region(region_lst, summary_lst):
+
+    """this function reads and compiles per target coverage output from MosDepth"""
+
     df_lst_for_avg = []
     df_lst_for_all_sample = []
     for r in region_lst:
-        sample_name = r.split('/')[-1].split('.')[0]
-        df = pd.read_csv( r, compression='gzip', sep='\t' , header=None, names=['chr', 'start', 'stop', 'target', 'average'])
-        tdf = df[['target', 'average']]
+        sample_name = r.split("/")[-1].split(".")[0]
+        df = pd.read_csv(
+            r,
+            compression="gzip",
+            sep="\t",
+            header=None,
+            names=["chr", "start", "stop", "target", "average"],
+        )
+        tdf = df[["target", "average"]]
         df_lst_for_avg.append(tdf)
-        sdf = df[['target', 'average']]
-        sdf.columns = ['target', f'{sample_name}']
-        sdf.set_index('target', inplace=True)
+        sdf = df[["target", "average"]]
+        sdf.columns = ["target", f"{sample_name}"]
+        sdf.set_index("target", inplace=True)
         df_lst_for_all_sample.append(sdf)
 
     avg_mdf = pd.concat(df_lst_for_avg, axis=0)
     # print(mdf)
-    gdf = avg_mdf.groupby('target').mean()
+    gdf = avg_mdf.groupby("target").mean()
     gdf.reset_index(inplace=True)
     # print(gdf)
 
@@ -30,50 +41,60 @@ def read_region(region_lst, summary_lst):
 
     sdf_lst = []
     for s in summary_lst:
-        sample_name = s.split('/')[-1].split('.')[0]
-        df = pd.read_csv( s, sep='\t' , header=0)
-        df['sample_name'] = sample_name
-        df = df[df['chrom'] == "total_region"]
-        df = df[['sample_name', 'length', 'mean', 'min', 'max']]
-        # print(df)
+        sample_name = s.split("/")[-1].split(".")[0]
+        df = pd.read_csv(s, sep="\t", header=0)
+        df["sample_name"] = sample_name
+        df = df[df["chrom"] == "total_region"]
+        df = df[["sample_name", "length", "mean", "min", "max"]]
         sdf_lst.append(df)
     smdf = pd.concat(sdf_lst, axis=0)
-    # print(smdf)
-
-
 
     return sample_mdf, gdf, smdf
 
+
 def read_threshold(threshold_lst):
-    """This reads and compiles .threshold.bed.gz file from the mosdepth output"""
+
+    """This function reads and compiles .threshold.bed.gz file from the mosdepth output"""
 
     df_lst = []
     sdf_lst = []
     for t in threshold_lst:
-        sample_name = t.split('/')[-1].split('.')[0]
-        df = pd.read_csv( t, compression='gzip', sep='\t' , header=0)
-        df['sample_name'] = sample_name
-        df['region_size'] = df['end'] - df['start']
+        sample_name = t.split("/")[-1].split(".")[0]
+        df = pd.read_csv(t, compression="gzip", sep="\t", header=0)
+        df["sample_name"] = sample_name
+        df["region_size"] = df["end"] - df["start"]
 
-        sdf = df.groupby('sample_name').sum()
-        sdf['%Over10X'] = sdf['10X'] / sdf['region_size']
-        sdf['%Over20X'] = sdf['20X'] / sdf['region_size']
-        sdf['%Over50X'] = sdf['50X'] / sdf['region_size']
-        sdf['%Over100X'] = sdf['100X'] / sdf['region_size']
-        sdf['%Over500X'] = sdf['500X'] / sdf['region_size']
-        sdf['%Over1000X'] = sdf['1000X'] / sdf['region_size']
-        sdf = sdf[['%Over10X', '%Over20X', '%Over50X', '%Over100X', '%Over500X','%Over1000X']]
+        sdf = df.groupby("sample_name").sum()
+        sdf["%Over10X"] = sdf["10X"] / sdf["region_size"]
+        sdf["%Over20X"] = sdf["20X"] / sdf["region_size"]
+        sdf["%Over50X"] = sdf["50X"] / sdf["region_size"]
+        sdf["%Over100X"] = sdf["100X"] / sdf["region_size"]
+        sdf["%Over500X"] = sdf["500X"] / sdf["region_size"]
+        sdf["%Over1000X"] = sdf["1000X"] / sdf["region_size"]
+        sdf = sdf[
+            ["%Over10X", "%Over20X", "%Over50X", "%Over100X", "%Over500X", "%Over1000X"]
+        ]
         # print(sdf)
         sdf_lst.append(sdf)
 
-        df['%Over10X'] = df['10X'] / df['region_size']
-        df['%Over20X'] = df['20X'] / df['region_size']
-        df['%Over50X'] = df['50X'] / df['region_size']
-        df['%Over100X'] = df['100X'] / df['region_size']
-        df['%Over500X'] = df['500X'] / df['region_size']
-        df['%Over1000X'] = df['1000X'] / df['region_size']
-        
-        df = df[['region', '%Over10X', '%Over20X', '%Over50X', '%Over100X', '%Over500X','%Over1000X']]
+        df["%Over10X"] = df["10X"] / df["region_size"]
+        df["%Over20X"] = df["20X"] / df["region_size"]
+        df["%Over50X"] = df["50X"] / df["region_size"]
+        df["%Over100X"] = df["100X"] / df["region_size"]
+        df["%Over500X"] = df["500X"] / df["region_size"]
+        df["%Over1000X"] = df["1000X"] / df["region_size"]
+
+        df = df[
+            [
+                "region",
+                "%Over10X",
+                "%Over20X",
+                "%Over50X",
+                "%Over100X",
+                "%Over500X",
+                "%Over1000X",
+            ]
+        ]
         # print(df.head())
 
         df_lst.append(df)
@@ -86,67 +107,64 @@ def read_threshold(threshold_lst):
         # df_lst.append(des_df)
 
     mdf = pd.concat(df_lst, axis=0)
-    gdf = mdf.groupby('region').mean()
+    gdf = mdf.groupby("region").mean()
     gdf.reset_index(inplace=True)
-    # print(gdf)
 
     msdf = pd.concat(sdf_lst, axis=0)
     msdf.reset_index(inplace=True)
-    # print(msdf)
 
     return gdf, msdf
+
 
 def read_lst(txt):
     lst = []
     with open(txt) as f:
         for line in f:
             lst.append(line.strip())
-    return lst 
-    
-def compile_exonStat(run_lst):
-    df_lst = []
-    for run in run_lst:
-        exonStat_glob = glob.glob(f'{run}/Aligned_Panel_9000_SGE/Project_9000_HGMDNext/Sample_RD_*/RD_*.coverage.exonStat')
-        exonStat_glob = [g for g in exonStat_glob if 'NEG' not in g]
-        # print(len(exonStat_glob))
-        for e in exonStat_glob:
-            df = pd.read_csv( e, skiprows=5)
-            print(df.head())
-            df_lst.append(df)
-    mdf = pd.concat(df_lst, axis=0)
-    gdf = mdf.groupby(['Gene','Isoform','Coding exon(CDS)']).mean()
-    print(gdf)
-    gdf.reset_index(inplace=True)
-    gdf.to_excel('exonStat.xlsx', index=False)
+    return lst
+
 
 def run():
-    print('starting to compile')
-    writer = pd.ExcelWriter(f'PerTargetMeanCov.xlsx', engine='xlsxwriter')
+
+    print("starting to compile")
+
+    writer = pd.ExcelWriter(f"PerTargetMeanCov.xlsx", engine="xlsxwriter")
     mdf, gdf, smdf = read_region(region_glob, summary_glob)
-    cols = [ c for c in mdf.columns if 'neg' not in c.lower() ]
+    cols = [c for c in mdf.columns if "neg" not in c.lower()]
     no_neg_mdf = mdf[cols]
+
     # df < 20 will turn any cells => 20 to NaN, and dropna.all will remove the row if it is all NaN( all > 20), previous cov threshold was set to 20
-    print(f'filtering targets below {cov_threshold}X')
-    low_cov_df = no_neg_mdf[no_neg_mdf < int(cov_threshold)].dropna(how='all')
+    print(f"filtering targets below {cov_threshold}X")
+
+    low_cov_df = no_neg_mdf[no_neg_mdf < int(cov_threshold)].dropna(how="all")
     low_cov_df.reset_index(inplace=True)
-    low_cov_df_target_names = low_cov_df[['target']]
-    below_cov_threshold_df = pd.merge(no_neg_mdf, low_cov_df_target_names, on='target', how='inner') 
-    mdf.to_excel(writer, sheet_name = 'All_Samples' ,index = True)
-    below_cov_threshold_df.to_excel(writer, sheet_name = f'TargetsBelow_{cov_threshold}X' ,index = False)
+    low_cov_df_target_names = low_cov_df[["target"]]
 
-    gdf.to_excel(writer, sheet_name = 'AvgAcrossSamples' ,index = False)
-    smdf.to_excel(writer, sheet_name = 'SampleLevel' ,index = False)
+    below_cov_threshold_df = pd.merge(
+        no_neg_mdf, low_cov_df_target_names, on="target", how="inner"
+    )
+
+    mdf.to_excel(writer, sheet_name="All_Samples", index=True)
+
+    below_cov_threshold_df.to_excel(
+        writer, sheet_name=f"TargetsBelow_{cov_threshold}X", index=False
+    )
+
+    gdf.to_excel(writer, sheet_name="AvgAcrossSamples", index=False)
+    smdf.to_excel(writer, sheet_name="SampleLevel", index=False)
     writer.save()
-    print('done with per target coverage')
+    
+    print("done with per target coverage")
 
-
-    writer = pd.ExcelWriter(f'PerTargetThresholdCov.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter(f"PerTargetThresholdCov.xlsx", engine="xlsxwriter")
     tgdf, tmsdf = read_threshold(threshold_glob)
-    tgdf.to_excel(writer, sheet_name = 'AvgAcrossSamples' ,index = False)
-    tmsdf.to_excel(writer, sheet_name = 'SampleLevel' ,index = False)
+    tgdf.to_excel(writer, sheet_name="AvgAcrossSamples", index=False)
+    tmsdf.to_excel(writer, sheet_name="SampleLevel", index=False)
+
     writer.save()
-    print('done with threshold coverage')
-    
-    
-if __name__=="__main__":
+
+    print("done with threshold coverage")
+
+
+if __name__ == "__main__":
     run()
--- per_base_convert.py
+++ per_base_convert.py
@@ -1,43 +1,54 @@
 import pandas as pd
-import sys, os, glob
+import sys, os
+
+"""This script converts per base coverage output from Mosdepth to the format 
+    that is used in GATK"""
 
 bedtools_output = sys.argv[1]
-# bedtools_output = '/mnt/dlin/share/REPO/common_scripts_dl/MosDepth/210528_NB501613_0394_AHK5WFBGXH/RD_21_127030_S1A.bedtool.out'
+
 def read_bedtools_output(pb):
 
-    df = pd.read_csv(pb, sep='\t', names=['chr', 'start', 'stop', 'cov'])
-    df['length'] = df['stop'] - df['start']
-    cleaned_df1 = df[((df['chr'] != 'artifact_seq') & ( df['length']<=300)) 
+    """This function reads bedtools ouput and do the inial parsing and cleaning"""
+
+    df = pd.read_csv(pb, sep="\t", names=["chr", "start", "stop", "cov"])
+    df["length"] = df["stop"] - df["start"]
+    cleaned_df1 = df[
+        ((df["chr"] != "artifact_seq") & (df["length"] <= 300))
         # & (df['cov']<20)
         # & (df['length'] > 1)
-        ]
-    
-    print(cleaned_df1)
+    ]
+
     return cleaned_df1
 
+
 def adding_per_base(df):
+
+    """This function iterate through MosDepth per base cov range result and generate a per base resolutoin coverage file"""
+
     cord_lst = []
     for i, row in df.iterrows():
-        chr = row['chr']
-        start = row['start']
-        stop = row['stop']
-        cov = row['cov']
-        length = row['length']
+        chr = row["chr"]
+        start = row["start"]
+        stop = row["stop"]
+        cov = row["cov"]
+        length = row["length"]
         if length == 1:
-            cord_lst.append([chr, start+1, cov])
+            cord_lst.append([chr, start + 1, cov])
         if length > 1:
             for i in range(length):
-                cord_lst.append([chr, start+1+i, cov])
-    updated_df = pd.DataFrame(cord_lst,columns=['chr', 'position', 'coverage'])
+                cord_lst.append([chr, start + 1 + i, cov])
+    updated_df = pd.DataFrame(cord_lst, columns=["chr", "position", "coverage"])
+
     return updated_df
 
+
 def run():
-    sample_name = os.path.basename(bedtools_output).split('.')[0]
+    
+    sample_name = os.path.basename(bedtools_output).split(".")[0]
     output_df = read_bedtools_output(bedtools_output)
-
     df = adding_per_base(output_df)
-    # print(df)
-    df.to_csv(f'{sample_name}.coverage', sep='\t', index=False, header=False)
+    df.to_csv(f"{sample_name}.coverage", sep="\t", index=False, header=False)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     run()
